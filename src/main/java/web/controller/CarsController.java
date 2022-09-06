8a6028f1464144de2b23984b78619e1235eb505e
Salavat Aksarin <theowalcott9@gmail.com>
2022-09-06 13:04:50 +0300

    Initial commit

---
+++ src/main/java/web/controller/CarsController.java
@@ -0,0 +1,32 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.model.Car;
+import web.service.CarService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class CarsController {
+
+	@GetMapping(value = "/cars")
+	public String cars(@RequestParam(value = "count", defaultValue = "5") int allCars, Model model) {
+		List<Car> list = new ArrayList<>();
+
+		list.add(new Car("Golf", "Blue", 2101));
+		list.add(new Car("BMV", "White", 325));
+		list.add(new Car("Mercedes", "Black", 124));
+		list.add(new Car("Mercedes", "Red", 60));
+		list.add(new Car("BMW", "Yellow", 720));
+		list.add(new Car("BMW", "Yellow", 720));
+		list = CarService.carsCount(list, allCars);
+		model.addAttribute("list", list);
+		return "car";
+	}
+	
+}
